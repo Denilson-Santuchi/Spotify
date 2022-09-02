e1ba7eda884399886ae42b0e3b01d7ac28889edb
Denilson Santuchi <denilsonsantuchi@hotmail.com>
2022-09-02 17:28:39 -0300

    add login form

--- spotify-project/src/pages/Login.tsx
+++ spotify-project/src/pages/Login.tsx
@@ -1,5 +1,24 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 export const Login = () => {
   return (
-    <h1>Hello Word!</h1>
-  )
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Seu nome de usúario" />
+        <Form.Text className="text-muted">
+          Você será chamado por esse nome.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Senha</Form.Label>
+        <Form.Control type="password" placeholder="Sua senha" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Entrar
+      </Button>
+    </Form>
+  );
 }
