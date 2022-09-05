87bfa94c287d06cc9d530ce6e8932feae0712d94
Denilson Santuchi <denilsonsantuchi@hotmail.com>
2022-09-05 18:17:12 -0300

    create search form

--- spotify-project/src/pages/Search.tsx
+++ spotify-project/src/pages/Search.tsx
@@ -1,10 +1,31 @@
-import { Header } from "../components"
+import { useState } from 'react';
+import { Header } from '../components'
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export const Search = () => {
+  const [artist, setArtist] = useState('');
+
   return (
     <section>
       <Header />
-      <div>Search</div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="text"
+            placeholder="Nome do Artista ou Banda"
+            onChange={({ target }) => setArtist(target.value)}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="button"
+          disabled={artist.length >= 2 ? false : true}
+        >
+          Pesquisar
+        </Button>
+      </Form>
     </section>
   )
 }
